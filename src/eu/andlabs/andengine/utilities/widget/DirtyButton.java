9431a24da151831c8eec8a80362e92352579c319
Johannes Borchardt <borchardt@andlabs.com>
2013-12-17 15:33:31 +0100

    Small improvements on DirtyButton

--- src/eu/andlabs/andengine/utilities/widget/DirtyButton.java
+++ src/eu/andlabs/andengine/utilities/widget/DirtyButton.java
@@ -1,5 +1,6 @@
 package eu.andlabs.andengine.utilities.widget;
 
+import org.andengine.entity.IEntity;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.input.touch.TouchEvent;
@@ -26,7 +27,7 @@ public class DirtyButton {
 
 	private OnClickListener mListener;
 
-	private Scene mScene;
+	private IEntity mParentEntity;
 
 	public DirtyButton(final int pId, final float pX, final float pY,
 			final TextureRegion pStateInitial,
@@ -38,6 +39,7 @@ public class DirtyButton {
 
 		this.mPressed = new Sprite(pX, pY, pStatePressed,
 				pVertexBufferObjectManager);
+		this.mPressed.setVisible(false);
 
 		this.mInitial = new Sprite(pX, pY, pStateInitial,
 				pVertexBufferObjectManager) {
@@ -46,20 +48,15 @@ public class DirtyButton {
 			public boolean onAreaTouched(TouchEvent pSceneTouchEvent,
 					float pPTouchAreaLocalX, float pPTouchAreaLocalY) {
 				if (pSceneTouchEvent.getMotionEvent().getAction() == MotionEvent.ACTION_DOWN) {
-					if (mPressed.getParent() == null) {
-						mScene.attachChild(mPressed);
-					}
-					mInitial.detachSelf();
+					DirtyButton.this.mPressed.setVisible(true);
+					DirtyButton.this.mInitial.setVisible(false);
 				} else if (pSceneTouchEvent.getMotionEvent().getAction() == MotionEvent.ACTION_UP) {
-					if (mInitial.getParent() == null) {
-						mScene.attachChild(mInitial);
-					}
+				    DirtyButton.this.mInitial.setVisible(true);
+				    DirtyButton.this.mPressed.setVisible(false);
 
-					if (mListener != null) {
-						mListener.onClick(mId);
+					if (DirtyButton.this.mListener != null) {
+					    DirtyButton.this.mListener.onClick(mId);
 					}
-
-					mPressed.detachSelf();
 				}
 				return true;
 			}
@@ -81,26 +78,36 @@ public class DirtyButton {
 	}
 
 	public void attachToScene(final Scene pScene) {
-		pScene.attachChild(mInitial);
-		pScene.registerTouchArea(mInitial);
+	    pScene.attachChild(this.mInitial);
+		pScene.registerTouchArea(this.mInitial);
+		pScene.attachChild(this.mPressed);
 
-		this.mScene = pScene;
+		this.mParentEntity = pScene;
 	}
+	
+	public void attachToEntity(final Scene pScene, final IEntity pEntity) {
+        pEntity.attachChild(this.mInitial);
+        pScene.registerTouchArea(this.mInitial);
+        pEntity.attachChild(this.mPressed);
 
+        this.mParentEntity = pEntity;
+    }
+    
+	
 	public float getWidth() {
-		return mInitial.getWidth();
+		return this.mInitial.getWidth();
 	}
 
 	public float getHeight() {
-		return mInitial.getHeight();
+		return this.mInitial.getHeight();
 	}
 
 	public float getX() {
-		return mX;
+		return this.mX;
 	}
 
 	public float getY() {
-		return mY;
+		return this.mY;
 	}
 
 	public void setRotation(final float pRotation) {
@@ -109,13 +116,13 @@ public class DirtyButton {
 	}
 
 	public void setFlippedVertical(boolean pVertical) {
-		mInitial.setFlippedVertical(pVertical);
-		mPressed.setFlippedVertical(pVertical);
+		this.mInitial.setFlippedVertical(pVertical);
+		this.mPressed.setFlippedVertical(pVertical);
 	}
 
 	public void setFlippedHorizontal(boolean pHorizontal) {
-		mInitial.setFlippedHorizontal(pHorizontal);
-		mPressed.setFlippedHorizontal(pHorizontal);
+	    this.mInitial.setFlippedHorizontal(pHorizontal);
+	    this.mPressed.setFlippedHorizontal(pHorizontal);
 	}
 
 	public void setFlipped(final boolean pHorizontal, final boolean pVertical) {
